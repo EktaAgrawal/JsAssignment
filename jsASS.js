8f52c0d0d983c8a4cc1fafb494d89986d453c943
EktaAgrawal <89682661+EktaAgrawal@users.noreply.github.com>
2021-11-26 20:04:27 +0530

    Add files via upload

---
+++ jsASS.js
@@ -0,0 +1,21 @@
+var name1 = {
+    first: "Ekta",
+    last: "Agrawal",
+    add: "Patna, Bihar"
+};
+var name2 = {
+    first: "shivani",
+    last: "Parmar",
+    add: "Indore, MP"
+};
+var name3 = {
+    first: "Richa",
+    last: "Kumari",
+    add: "Patna, Bihar"
+};
+
+
+
+console.log(name1);
+console.log(name2);
+console.log(name3);
